0b93156b4320434f4d69f5194ba12a2694249703
Suryabhan17 <suryabhanuday99@gmail.com>
2025-10-24 15:58:35 +0530

    file udate

--- add.java
+++ add.java
@@ -2,9 +2,10 @@
 public class add{
   public static void main(String args[]){
     System.out.println("Suryabhan");
-    System.out.println("Surya")
+    System.out.println("Surya");
 
     System.out.println("124578");
+    System.out.println("Suryabhan45");
     
 
   }
